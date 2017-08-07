751eea45ac9abd5231d1c5ddc7650f2e0af2f148
Alfred Osorio <aosorio@Alfreds-MacBook-Pro.local>
2017-08-07 20:46:47 +1000

    Author: Setup controller methods

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,42 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :update, :destroy]
+
+  # GET /authors
+  def index
+    @authors = Author.all
+    json_response(@authors)
+  end
+
+  # POST /authors
+  def create
+    @authors = Author.create!(author_params)
+    json_response(@author, :created)
+  end
+
+  # GET /authors/:id
+  def show
+    json_response(@author)
+  end
+
+  # PUT /authors/:id
+  def update
+    @author.update(author_params)
+    head :no_content
+  end
+
+  # DELETE /authors/:id
+  def destroy
+    @author.destroy
+    head :no_content
+  end
+
+  private
+    def author_params
+      # Whitelist params
+      params.permit(:firstname, :lastname)
+    end
+
+    def set_author
+      @author = Author.find(params[:id])
+    end
 end
